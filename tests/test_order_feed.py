25d18a6c81d0c2ac964d5344db7f8387f34db486
Marina Laskova <marinalaskova4@gmail.com>
2026-08-13 17:28:42 +0300

    Создан файл helper.py

--- tests/test_order_feed.py
+++ tests/test_order_feed.py
@@ -1,6 +1,5 @@
 import allure
-import requests
-from endpoints import ORDERS
+from helpers.helper import OrderApiHelper
 
 @allure.suite("Раздел 'Лента заказов'")
 class TestOrderFeed:
@@ -11,11 +10,8 @@ class TestOrderFeed:
         feed_page.navigate("/feed")
         old_count = feed_page.get_all_time_orders()
         
-        headers = {"Authorization": token}
-        payload = {"ingredients": get_ingredient_hashes}
-        
-        with allure.step("Создание заказа через API для увеличения счетчиков"):
-            requests.post(ORDERS, json=payload, headers=headers)
+        # Вызов изолированного хелпера создания заказа
+        OrderApiHelper.create_order(token, get_ingredient_hashes)
             
         feed_page.navigate("/feed")
         feed_page.wait_for_all_time_orders_increase(old_count)
@@ -28,11 +24,8 @@ class TestOrderFeed:
         feed_page.navigate("/feed")
         old_count = feed_page.get_today_orders()
         
-        headers = {"Authorization": token}
-        payload = {"ingredients": get_ingredient_hashes}
-        
-        with allure.step("Создание заказа через API для увеличения счетчиков"):
-            requests.post(ORDERS, json=payload, headers=headers)
+        # Вызов изолированного хелпера создания заказа
+        OrderApiHelper.create_order(token, get_ingredient_hashes)
             
         feed_page.navigate("/feed")
         feed_page.wait_for_today_orders_increase(old_count)
@@ -42,11 +35,9 @@ class TestOrderFeed:
     @allure.title("Появление номера нового заказа в разделе 'В работе'")
     def test_new_order_appears_in_progress(self, main_page, feed_page, created_user, get_ingredient_hashes):
         _, token = created_user
-        headers = {"Authorization": token}
-        payload = {"ingredients": get_ingredient_hashes}
         
-        with allure.step("Создание заказа через API для вывода на доску в работе"):
-            requests.post(ORDERS, json=payload, headers=headers)
+        # Вызов изолированного хелпера создания заказа
+        OrderApiHelper.create_order(token, get_ingredient_hashes)
             
         feed_page.navigate("/feed")
         feed_page.wait_for_order_to_appear_in_progress()
